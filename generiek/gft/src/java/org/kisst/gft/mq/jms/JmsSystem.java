03ef6eacc25020a535110c82370a0b83f1a26167
Mark Hooijkaas <mark@kisst.org>
2010-02-11 09:13:22 +0000

    added mq stuff

--- generiek/gft/src/java/org/kisst/gft/mq/jms/JmsSystem.java
+++ generiek/gft/src/java/org/kisst/gft/mq/jms/JmsSystem.java
@@ -4,28 +4,31 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.kisst.cfg4j.Props;
 import org.kisst.gft.mq.MqQueue;
 import org.kisst.gft.mq.MqSystem;
 
-public class JmsSystem implements MqSystem {
-	private final Props props;
+public abstract class JmsSystem implements MqSystem {
+	protected final Props props;
 	private final Connection connection;
 	
 	public JmsSystem(Props props) {
 		this.props=props;
-		String user=props.getString("user",null);
-		String password=props.getString("password",null);
-		String url=props.getString("url");
 		try {
-			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(user, password, url);
-			connection = connectionFactory.createConnection();
+			ConnectionFactory connectionFactory = createConnectionFactory();
+			String username=props.getString("username", null);
+			String password=props.getString("password", null);
+			if (username==null)
+				connection = connectionFactory.createConnection();
+			else
+				connection = connectionFactory.createConnection(username, password);
 			connection.start();
 		}
 		catch (JMSException e) {throw new RuntimeException(e); }
 	}
 
+	abstract protected ConnectionFactory createConnectionFactory();
+	
 	public MqQueue getQueue(String name) { return new JmsQueue(this, props.getProps("queue."+name)); }
 	public Connection getConnection() { return connection;	}
 	public void close() {
