9ba94046769ee327b5c0e69f6a54e4716bcc0ff1
Mark Hooijkaas <mark-git@hooijkaas.net>
2010-02-18 11:32:23 +0100

    Added sshhosts and renamed hosts to httphosts

--- src/java/org/kisst/gft/action/SshAction.java
+++ src/java/org/kisst/gft/action/SshAction.java
@@ -3,7 +3,7 @@ package org.kisst.gft.action;
 import org.kisst.cfg4j.Props;
 import org.kisst.gft.GftContainer;
 import org.kisst.gft.filetransfer.FileTransferData;
-import org.kisst.gft.filetransfer.Ssh;
+import org.kisst.gft.filetransfer.SshHost;
 import org.kisst.gft.task.Task;
 import org.kisst.util.StringUtil;
 import org.slf4j.Logger;
@@ -13,17 +13,19 @@ public class SshAction implements Action {
 	private final static Logger logger=LoggerFactory.getLogger(SshAction.class);
 	private final Props actionProps;
 	private final String commandTemplate;
+	private final SshHost host;
 	
 	public SshAction(GftContainer gft, Props props) {
 		this.actionProps=props;
 		commandTemplate =props.getString("command");
+		host=gft.sshhosts.get(props.getString("host"));
 	}
 
 	public Object execute(Task task) {
 		FileTransferData ft= (FileTransferData) task.getData();
 		String command=StringUtil.substitute(commandTemplate, ft.getProps(actionProps));
-
-		String result=Ssh.ssh(ft.channel.cred, ft.channel.getHost(), command);
+		logger.info("ssh call to {} with command {}", host, command);
+		String result=host.call(command);
 		logger.info("ssh result {}",result);
 		return null;
 	}
