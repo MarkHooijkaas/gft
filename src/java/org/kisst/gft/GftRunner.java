942faf134ee9f4ed99afa512d39a5884ebeffa55
Mark Hooijkaas <mark-git@hooijkaas.net>
2010-04-24 18:44:47 +0200

    some small fixes

--- src/java/org/kisst/gft/GftRunner.java
+++ src/java/org/kisst/gft/GftRunner.java
@@ -3,12 +3,13 @@ package org.kisst.gft;
 import java.io.File;
 
 import org.apache.log4j.PropertyConfigurator;
+import org.kisst.gft.ssh.GenerateKey; 
 
 public class GftRunner {
 	private final File configfile;
 	private boolean running=false;
 	private GftContainer gft;
-	
+
 	public GftRunner(File configfile) {
 		this.configfile = configfile;
 	}
@@ -39,11 +40,12 @@ public class GftRunner {
 			return;
 		gft.stop();
 	}
-	
+
 
 	private static Cli cli=new Cli();
 	private static Cli.StringOption config = cli.stringOption("c","config","configuration file", "conf/gft.props");
 	private static Cli.Flag help =cli.flag("h", "help", "show this help");
+	private static Cli.Flag keygen =cli.flag("k", "keygen", "generate a public/private keypair"); 
 	public static void main(String[] args) {
 		cli.parse(args);
 		if (help.isSet()) {
@@ -51,10 +53,14 @@ public class GftRunner {
 			return;
 		}
 		File configfile=new File(config.get());
+		if (keygen.isSet()) {
+			GenerateKey.generateKey(configfile.getParentFile().getAbsolutePath()+"/keyfiles/id_dsa_gft");
+			return;
+		}
 		PropertyConfigurator.configure(configfile.getParent()+"/log4j.properties");
 		GftRunner runner= new GftRunner(configfile);
 		runner.run();
-		
+
 		System.out.println("GFT stopped");
 	}
 
