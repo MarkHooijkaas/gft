4ebd74d4e4c979c2fc87caa8f1501784d921be28
Mark Hooijkaas <mark@kisst.org>
2010-04-26 12:06:38 +0000

    fix permissions on a AS/400 specific way

--- generiek/gft/src/java/org/kisst/gft/action/FixPermissions.java
+++ generiek/gft/src/java/org/kisst/gft/action/FixPermissions.java
@@ -1,13 +1,22 @@
 package org.kisst.gft.action;
 
+import org.kisst.cfg4j.Props;
+import org.kisst.gft.GftContainer;
 import org.kisst.gft.filetransfer.FileTransferData;
 import org.kisst.gft.task.Task;
 
 public class FixPermissions implements Action {
 
+	private final String otherUser;
+	
+	public FixPermissions (GftContainer gft, Props props) {
+		otherUser =props.getString("otherUser");
+	}
+
+
 	public Object execute(Task task) {
 		FileTransferData ft= (FileTransferData) task.getData();
-		ft.channel.dest.call("chmod a+w "+ft.channel.srcdir+"/"+ft.file);
+		ft.channel.dest.call("system chgaut \"obj('"+ft.channel.destdir+"/"+ft.file+"') user("+otherUser+") DTAAUT(*RWX)\"");
 		return null;
 	}
 
