7ada85ce69fd0bb2e38a186173aae35ce8a24a16
Mark Hooijkaas <mark@kisst.org>
2010-04-29 13:22:44 +0000

    Fix AS/400 permissions using authlist

--- generiek/gft/src/java/org/kisst/gft/action/FixPermissions.java
+++ generiek/gft/src/java/org/kisst/gft/action/FixPermissions.java
@@ -1,22 +1,22 @@
 package org.kisst.gft.action;
 
-import org.kisst.cfg4j.Props;
-import org.kisst.gft.GftContainer;
 import org.kisst.gft.filetransfer.FileTransferData;
 import org.kisst.gft.task.Task;
 
 public class FixPermissions implements Action {
 
-	private final String otherUser;
-	
-	public FixPermissions (GftContainer gft, Props props) {
-		otherUser =props.getString("otherUser");
-	}
-
-
 	public Object execute(Task task) {
 		FileTransferData ft= (FileTransferData) task.getData();
-		ft.channel.dest.call("system chgaut \"obj('"+ft.channel.destdir+"/"+ft.file+"') user("+otherUser+") DTAAUT(*RWX)\"");
+		String s=ft.channel.dest.call("system dspaut \"obj('"+ft.channel.destdir+"/')\"");
+		int pos=s.indexOf("Lijst van machtigingen");
+		if (pos<=0)
+			throw new RuntimeException("Kan geen lijst van machtingen vinden voor directory "+ft.channel.destdir);
+		pos=s.indexOf(":", pos);
+		int pos2 = s.indexOf("\n",pos);
+		if (pos<=0 || pos2<=0 || pos>pos2)
+			throw new RuntimeException("Problem parsing dspaut output: "+s);
+		String autlist=s.substring(pos+1, pos2).trim();
+		ft.channel.dest.call("system chgaut \"obj('"+ft.channel.destdir+"/"+ft.file+"') autl("+autlist+")\"");
 		return null;
 	}
 
