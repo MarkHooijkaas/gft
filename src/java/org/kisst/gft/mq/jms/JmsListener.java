593e52e1868111d1a9a0e4d8a976af30e6f8b282
Mark Hooijkaas <mark-git@hooijkaas.net>
2010-08-21 16:02:42 +0200

    Moved the clone method to JmsUtil

--- src/java/org/kisst/gft/mq/jms/JmsListener.java
+++ src/java/org/kisst/gft/mq/jms/JmsListener.java
@@ -1,8 +1,5 @@
 package org.kisst.gft.mq.jms;
 
-import java.util.Enumeration;
-
-import javax.jms.BytesMessage;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -214,7 +211,7 @@ public class JmsListener implements QueueListener, Representable {
 						queue=retryqueue;
 					Destination errordestination = session.createQueue(queue+system.sendParams);
 					MessageProducer producer = session.createProducer(errordestination);
-					Message errmsg=cloneMessage(message);
+					Message errmsg=JmsUtil.cloneMessage(session, message);
 					producer.send(errmsg);
 					
 					producer.close();
@@ -231,44 +228,5 @@ public class JmsListener implements QueueListener, Representable {
 			}
 		}
 
-
-		private Message cloneMessage( Message src ) {
-			try {
-				Message dest;
-				if( src instanceof BytesMessage ) {
-					dest = session.createBytesMessage();
-					int len = (int) ((BytesMessage) src ).getBodyLength();
-					byte[] msg = new byte[ len ];
-					( (BytesMessage) src ).readBytes( msg );
-					( (BytesMessage) dest ).writeBytes( msg );
-
-				} 
-				else if( src instanceof TextMessage ) {
-					dest = session.createTextMessage();
-					( (TextMessage) dest).setText( ( (TextMessage) src).getText() );
-				}
-				else  
-					throw new RuntimeException( "Unsupported message format: "+ src.getClass().getName() );
-
-				if( src.getJMSMessageID() != null ) dest.setJMSMessageID( src.getJMSMessageID() );
-				if( src.getJMSCorrelationID() != null ) dest.setJMSCorrelationID( src.getJMSCorrelationID() );
-				if( src.getJMSReplyTo() != null ) dest.setJMSReplyTo( src.getJMSReplyTo() );
-				if( src.getJMSType() != null ) dest.setJMSType( src.getJMSType() );
-				dest.setJMSDeliveryMode( src.getJMSDeliveryMode() );
-				dest.setJMSExpiration( src.getJMSExpiration() );
-				dest.setJMSPriority( src.getJMSPriority() );
-				dest.setJMSRedelivered( src.getJMSRedelivered() );
-				dest.setJMSTimestamp( src.getJMSTimestamp() );
-
-				Enumeration<?> properties = src.getPropertyNames();
-				while( properties.hasMoreElements() ) {
-					String key = (String) properties.nextElement();
-					if( key.startsWith( "JMSX" ) ) continue;
-					dest.setObjectProperty( key, src.getObjectProperty( key ) );
-				} 
-				return dest;
-			}
-			catch (JMSException e) { throw new RuntimeException(e);}
-		}
 	}
 }
---
+++ src/java/org/kisst/gft/mq/jms/JmsUtil.java
@@ -0,0 +1,50 @@
+package org.kisst.gft.mq.jms;
+
+import java.util.Enumeration;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class JmsUtil {
+	public static Message cloneMessage(Session session, Message src ) {
+		try {
+			Message dest;
+			if( src instanceof BytesMessage ) {
+				dest = session.createBytesMessage();
+				int len = (int) ((BytesMessage) src ).getBodyLength();
+				byte[] msg = new byte[ len ];
+				( (BytesMessage) src ).readBytes( msg );
+				( (BytesMessage) dest ).writeBytes( msg );
+
+			} 
+			else if( src instanceof TextMessage ) {
+				dest = session.createTextMessage();
+				( (TextMessage) dest).setText( ( (TextMessage) src).getText() );
+			}
+			else  
+				throw new RuntimeException( "Unsupported message format: "+ src.getClass().getName() );
+
+			if( src.getJMSMessageID() != null ) dest.setJMSMessageID( src.getJMSMessageID() );
+			if( src.getJMSCorrelationID() != null ) dest.setJMSCorrelationID( src.getJMSCorrelationID() );
+			if( src.getJMSReplyTo() != null ) dest.setJMSReplyTo( src.getJMSReplyTo() );
+			if( src.getJMSType() != null ) dest.setJMSType( src.getJMSType() );
+			dest.setJMSDeliveryMode( src.getJMSDeliveryMode() );
+			dest.setJMSExpiration( src.getJMSExpiration() );
+			dest.setJMSPriority( src.getJMSPriority() );
+			dest.setJMSRedelivered( src.getJMSRedelivered() );
+			dest.setJMSTimestamp( src.getJMSTimestamp() );
+
+			Enumeration<?> properties = src.getPropertyNames();
+			while( properties.hasMoreElements() ) {
+				String key = (String) properties.nextElement();
+				if( key.startsWith( "JMSX" ) ) continue;
+				dest.setObjectProperty( key, src.getObjectProperty( key ) );
+			} 
+			return dest;
+		}
+		catch (JMSException e) { throw new RuntimeException(e);}
+	}
+}
