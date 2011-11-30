e7bf2f657f1cad8d701b2526efadb2e2778cc07c
Mark Hooijkaas <mark@kisst.org>
2011-11-30 14:01:43 +0000

    specifieke DasfTask gemaakt

---
+++ generiek/gft/src/java/org/kisst/gft/task/JmsTask.java
@@ -0,0 +1,20 @@
+package org.kisst.gft.task;
+
+import org.kisst.gft.GftContainer;
+import org.kisst.jms.JmsMessage;
+
+
+public class JmsTask extends BasicTask implements TextTask {
+	private final JmsMessage msg;
+	
+	public JmsTask(GftContainer gft, TaskDefinition taskdef, JmsMessage msg) {
+		super(gft, taskdef);
+		this.msg=msg;
+	}
+
+	public JmsMessage getJmsMessage() { return msg; }
+
+	@Override
+	public String getText() { return getJmsMessage().getData();} 
+	
+}
--- generiek/gft/src/java/org/kisst/gft/task/JmsXmlTask.java
+++ generiek/gft/src/java/org/kisst/gft/task/JmsXmlTask.java
@@ -5,19 +5,16 @@ import org.kisst.jms.JmsMessage;
 import org.kisst.util.XmlNode;
 
 
-public class JmsXmlTask extends BasicTask implements SoapTask {
-	private final JmsMessage msg;
+public class JmsXmlTask extends JmsTask implements SoapTask {
 	private final XmlNode message;
 	private final XmlNode content; 		
 	
 	public JmsXmlTask(GftContainer gft, TaskDefinition taskdef, JmsMessage msg) {
-		super(gft, taskdef);
-		this.msg=msg;
+		super(gft, taskdef, msg);
 		this.message=new XmlNode(msg.getData());
 		this.content=message.getChild("Body").getChildren().get(0);
 	}
 
-	public JmsMessage getJmsMessage() { return msg; } 
 	@Override public XmlNode getContent() { return content; }
 	@Override public XmlNode getMessage() { return message;}
 }
