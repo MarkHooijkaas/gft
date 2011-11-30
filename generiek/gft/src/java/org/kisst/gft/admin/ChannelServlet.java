d76900a5fe0d6b3b72bc758619d635581d872738
Mark Hooijkaas <mark@kisst.org>
2011-11-30 17:46:11 +0000

    added extra filtering to log servlet

--- generiek/gft/src/java/org/kisst/gft/admin/ChannelServlet.java
+++ generiek/gft/src/java/org/kisst/gft/admin/ChannelServlet.java
@@ -29,9 +29,16 @@ public class ChannelServlet extends BaseServlet {
 	
 	private void writeChannelInfo(PrintWriter out, Channel ch) {
 		out.println("<h1>Channel "+ch.getName()+"</h1>");
+		out.println("<h2>Directories</h2>");
 		out.println("<ul>");
-		out.println("<li>FROM: <a href=\"/dir/"+ch.src.getSshHost().name+"/"+ ch.srcdir +"\">"+ch.src.getSshHost().name+"/"+ ch.srcdir +"</a>");
-		out.println("<li>TO:   <a href=\"/dir/"+ch.dest.getSshHost().name+"/"+ch.destdir+"\">"+ch.dest.getSshHost().name+"/"+ch.destdir+"</a>");
+		out.println("<li>FROM: <a href=\"/dir/"+ch.src.getSshHost().name+"/"+ ch.srcdir +"\">"+ch.src.getSshHost().name +"/"+ch.src.getBasePath() + ch.srcdir +"</a>");
+		out.println("<li>TO:   <a href=\"/dir/"+ch.dest.getSshHost().name+"/"+ch.destdir+"\">"+ch.dest.getSshHost().name+"/"+ch.dest.getBasePath()+ ch.destdir+"</a>");
+		out.println("</ul>");
+
+		out.println("<h2>Logging</h2>");
+		out.println("<ul>");
+		out.println("<li><a href=\"/logging/days=1&channel="+ch.getName()+"\">ALL Logging</a>");
+		out.println("<li><a href=\"/logging/days=1&channel="+ch.getName()+"&level=error\">ERROR Logging</a>");
 		out.println("</ul>");
 		
 		out.println("<h2>Config</h2>");
