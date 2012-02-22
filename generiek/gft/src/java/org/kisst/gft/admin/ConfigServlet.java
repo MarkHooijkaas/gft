444a795f73cd99de8ea9b65d88ea792a98208b0b
Mark Hooijkaas <mark@kisst.org>
2012-02-22 10:37:08 +0000

    filter any lines containing the string password

--- generiek/gft/src/java/org/kisst/gft/admin/ConfigServlet.java
+++ generiek/gft/src/java/org/kisst/gft/admin/ConfigServlet.java
@@ -19,10 +19,23 @@ public class ConfigServlet extends BaseServlet {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.println("<pre>");
-		out.println(gft.props);
-		out.println(((SimpleProps)gft.props).toPropertiesString());
+		filteredOutput(out, gft.props.toString());
+		filteredOutput(out, ((SimpleProps)gft.props).toPropertiesString());
 		out.println("</pre>");
 		response.setStatus(HttpServletResponse.SC_OK);
 	}
 
+	
+	private void filteredOutput(PrintWriter out, String str) {
+		for (String line: str.split("[\n]")) {
+			if ( line.toLowerCase().contains("password")) {
+				int pos = line.indexOf('=');
+				if (pos<=0)
+					pos=line.toLowerCase().indexOf("password")+8;
+				line=line.substring(0,pos+1)+"***";
+			}
+			out.println(line);
+		}
+	}
+	
 }
