9580566951ea46bb9f5ef8b8e0fedb82628557c7
Mark Hooijkaas <mark@kisst.org>
2012-04-02 09:03:09 +0000

    made compatible with SoruceFile interface

--- generiek/gft/src/java/org/kisst/gft/filetransfer/action/CheckSourceFile.java
+++ generiek/gft/src/java/org/kisst/gft/filetransfer/action/CheckSourceFile.java
@@ -2,17 +2,16 @@ package org.kisst.gft.filetransfer.action;
 
 import org.kisst.gft.FunctionalException;
 import org.kisst.gft.action.Action;
-import org.kisst.gft.filetransfer.FileTransferTask;
 import org.kisst.gft.task.Task;
 
 public class CheckSourceFile implements Action {
 	public boolean safeToRetry() { return true; }
 
 	public Object execute(Task task) {
-		FileTransferTask ft= (FileTransferTask) task;
+		SourceFile src= (SourceFile) task;
 		// TODO: remember filesize
-		if (! ft.channel.src.fileExists(ft.srcpath))
-				throw new FunctionalException("Source file "+ft.srcpath+" does not exist or is not accessible");
+		if (! src.getSourceFileServer().fileExists(src.getSourceFilePath()))
+				throw new FunctionalException("Source file "+src.getSourceFilePath()+" does not exist or is not accessible");
 		return null;
 	}
 
--- generiek/gft/src/java/org/kisst/gft/filetransfer/action/DeleteSourceFile.java
+++ generiek/gft/src/java/org/kisst/gft/filetransfer/action/DeleteSourceFile.java
@@ -1,15 +1,14 @@
 package org.kisst.gft.filetransfer.action;
 
 import org.kisst.gft.action.Action;
-import org.kisst.gft.filetransfer.FileTransferTask;
 import org.kisst.gft.task.Task;
 
 public class DeleteSourceFile implements Action {
 	public boolean safeToRetry() { return true; }
 
 	public Object execute(Task task) {
-		FileTransferTask ft= (FileTransferTask) task;
-		ft.channel.src.deleteFile(ft.srcpath);
+		SourceFile src= (SourceFile) task;
+		src.getSourceFileServer().deleteFile(src.getSourceFilePath());
 		return null;
 	}
 
