20654715bfa2bc52d1880a13b07ad392e19fa29e
Mark Hooijkaas <mark@kisst.org>
2014-04-03 13:15:07 +0000

    cleanup of strange log_start etc steps to normal logging statements

--- generiek/gft/src/java/org/kisst/gft/task/BasicTaskDefinition.java
+++ generiek/gft/src/java/org/kisst/gft/task/BasicTaskDefinition.java
@@ -3,9 +3,11 @@ package org.kisst.gft.task;
 import java.lang.reflect.Constructor;
 
 import org.kisst.gft.GftContainer;
+import org.kisst.gft.LogService;
 import org.kisst.gft.action.Action;
 import org.kisst.gft.action.ActionList;
 import org.kisst.gft.filetransfer.Channel;
+import org.kisst.gft.filetransfer.FileTransferTask;
 import org.kisst.props4j.Props;
 import org.kisst.props4j.SimpleProps;
 import org.kisst.util.ReflectionUtil;
@@ -18,9 +20,7 @@ public abstract class BasicTaskDefinition implements TaskDefinition {
 	public final GftContainer gft;
 	public final String name;
 	protected final Action action;
-	protected Action startAction=null;
-	protected Action endAction=null;
-	protected Action errorAction=null;
+
 	public final Props props;
 	private final SimpleProps context;
 
@@ -39,17 +39,6 @@ public abstract class BasicTaskDefinition implements TaskDefinition {
 			this.action=flow;
 		else
 			this.action=new ActionList(this, props, defaultActions);
-		
-		SimpleProps actprops=new SimpleProps();
-
-		actprops.put("actions", "log_error");
-		this.errorAction=new ActionList(this, actprops);
-
-		actprops.put("actions", "log_start");
-		this.startAction=new ActionList(this, actprops);
-		
-		actprops.put("actions", "log_completed");
-		this.endAction=new ActionList(this, actprops);	
 	}
 
 	// This constructor is for backward compatibility 
@@ -65,18 +54,15 @@ public abstract class BasicTaskDefinition implements TaskDefinition {
 	public void run(Task task) {
 		try {
 			totalCount++;
-			if (startAction!=null)
-				startAction.execute(task);
+			logStart(task);
 			action.execute(task);
-			if (endAction!=null)
-				endAction.execute(task);
+			logCompleted(task);
 		}
 		catch (RuntimeException e) {
 			errorCount++;
 			task.setLastError(e);
 			try {
-				if (errorAction!=null)
-					errorAction.execute(task);
+				logError(task, e);
 			}
 			catch(RuntimeException e2) { 
 				logger.error("Could not perform the error actions ",e);
@@ -120,5 +106,27 @@ public abstract class BasicTaskDefinition implements TaskDefinition {
 		else
 			return (Action) ReflectionUtil.createObject(c, new Object[] {gft, props} );
 		
+	}
+	
+	
+	private void logStart(Task task) {
+		LogService.log("info", "start", task.getTaskDefinition().getName(), "started", "Started"+getLogDetails(task)); 
+	}
+	
+	private void logCompleted(Task task) {
+		LogService.log("info", "done", task.getTaskDefinition().getName(), "completed","Completed"+getLogDetails(task));
+	}
+	private void logError(Task task, RuntimeException e) {
+		String details = "Fout bij actie:"+task.getLastAction()+" fout:"+e.getMessage()+getLogDetails(task);
+		LogService.log("error", task.getLastAction(), task.getTaskDefinition().getName(), "error", details);
+	}
+	
+	private String getLogDetails(Task task) {
+		if (task instanceof FileTransferTask) {
+			FileTransferTask ft = (FileTransferTask) task;
+			return  "bestand: "+ft.srcpath+ ", van: "+ft.channel.src+"/"+ft.srcpath+" naar: "+ft.channel.dest+"/"+ft.destpath;
+		}
+		else
+			return "";
 	}
 }
