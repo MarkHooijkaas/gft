a1b9fe0a36651a06f6100a176d7403372a910a71
Mark Hooijkaas <mark@kisst.org>
2014-12-15 10:57:07 +0000

    Added possibility to move a single message identified by message id

--- src/java/org/kisst/mq/MsgMover.java
+++ src/java/org/kisst/mq/MsgMover.java
@@ -5,6 +5,9 @@ import java.io.File;
 import org.kisst.props4j.Props;
 import org.kisst.props4j.SimpleProps;
 
+
+import org.kisst.util.CryptoUtil;
+
 //import com.ibm.mq.MQC;
 import com.ibm.mq.MQException;
 import com.ibm.mq.MQGetMessageOptions;
@@ -16,16 +19,18 @@ import com.ibm.mq.constants.MQConstants;
 public class MsgMover
 {
 	public static void main(String[] args) {
-		if (args.length<2 || args.length>3)
-			throw new RuntimeException("Correct syntax java -jar MsgMover.jar srcqueue dstqueue [qmgr]");
+		if (args.length<2 || args.length>4)
+			throw new RuntimeException("Correct syntax java -jar MsgMover.jar srcqueue dstqueue [qmgr [msgid]]");
 		String srcqueue=args[0];
 		String destqueue=args[1];
 		File configfile=new File("MsgMover.properties");
 		SimpleProps props = new SimpleProps();
 		props.load(configfile);
 		try {
-			if (args.length==3)
-				MsgMover.moveAllMessages(props.getProps("qm."+args[2]), srcqueue, destqueue);
+			if (args.length>2)
+				props=(SimpleProps) props.getProps("qm."+args[2]);
+			if (args.length>3)
+				MsgMover.moveMessage(props, srcqueue, destqueue, args[3]);
 			else
 				MsgMover.moveAllMessages(props, srcqueue, destqueue);
 		} 
@@ -34,6 +39,14 @@ public class MsgMover
 
 
 	public static void moveAllMessages(Props props, String src, String dest) throws MQException {
+		moveMessage(props, src, dest, "@all");
+	}
+	
+	
+	public static void moveMessage(Props props, String src, String dest, String msgid) throws MQException {
+		if (msgid==null)
+			throw new RuntimeException("msgid==null is not allowed, use @all instead");
+		boolean moveAll=(msgid.equals("@all"));
 		QueueManager qm=new QueueManager(props);
 		MQQueue srcq = qm.getQueue(src, 
 				MQConstants.MQOO_INPUT_SHARED |
@@ -49,7 +62,8 @@ public class MsgMover
 					MQConstants.MQGMO_SYNCPOINT | // best practice MQConstants.MQGMO_SYNCPOINT_IF_PERSISTENT |
 					MQConstants.MQGMO_FAIL_IF_QUIESCING // for nice shutdown. Not really necessary, but best practice in general
 					; 
-			gmo.matchOptions=MQConstants.MQGMO_NONE;
+			if (! moveAll)
+				gmo.matchOptions = MQConstants.MQMO_MATCH_MSG_ID;
 
 			MQPutMessageOptions pmo = new MQPutMessageOptions();
 			pmo.options=MQConstants.MQPMO_SET_ALL_CONTEXT;
@@ -57,6 +71,8 @@ public class MsgMover
 			int i=0;
 			while (true) { //srcq.getCurrentDepth()>0) {    
 				MQMessage msg = new MQMessage();
+				if (! moveAll)
+					msg.messageId=CryptoUtil.fromHex(msgid.toUpperCase());
 				try {
 					srcq.get(msg, gmo);
 				}
@@ -67,18 +83,21 @@ public class MsgMover
 					}
 					throw e;
 				}
+				i++;
 				System.out.print("Moving message "+i+" ... ");
 				destq.put(msg, pmo); 
 				qm.commit();
 				System.out.println("OK");
-				i++;
 			}
+			System.out.println("Moved "+i+" messages");
 		}
 		finally {
 			srcq.close();
 			destq.close();
 			qm.close();
 		}
+
+
 	}
 
 }
