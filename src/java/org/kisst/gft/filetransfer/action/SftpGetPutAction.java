4dcef0039206bca6035c4b0c7b16a43e14480789
Mark Hooijkaas <mark@kisst.org>
2015-03-16 14:18:59 +0000

    first version of a combined get-put action (not working yet)

---
+++ src/java/org/kisst/gft/filetransfer/action/SftpGetPutAction.java
@@ -0,0 +1,60 @@
+/**
+Copyright 2008, 2009 Mark Hooijkaas
+
+This file is part of the RelayConnector framework.
+
+The RelayConnector framework is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The RelayConnector framework is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the RelayConnector framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kisst.gft.filetransfer.action;
+
+import org.kisst.gft.action.BaseAction;
+import org.kisst.gft.filetransfer.FileServerConnection;
+import org.kisst.gft.task.BasicTaskDefinition;
+import org.kisst.gft.task.Task;
+import org.kisst.props4j.Props;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SftpGetPutAction extends BaseAction {
+	private final static Logger logger=LoggerFactory.getLogger(SftpGetPutAction.class);
+
+	private final boolean resumeAllowed;
+	public SftpGetPutAction(BasicTaskDefinition taskdef, Props props) { 
+		super(taskdef, props, getSourceField(taskdef));
+		this.resumeAllowed=props.getBoolean("resumeAllowed", true);
+	}
+
+
+	public Object execute(Task task) {
+		SourceFile src= (SourceFile) task;
+		DestinationFile dest = (DestinationFile) task;
+
+		FileServerConnection srcfsconn=src.getSourceFile().getFileServer().openConnection();
+		FileServerConnection destfsconn=null;
+		try {
+			destfsconn=dest.getDestinationFile().getFileServer().openConnection();
+			String remotefile = src.getSourceFile().getPath();
+		}
+		finally {
+			if (srcfsconn!=null)
+				srcfsconn.close();
+			if (destfsconn!=null)
+				destfsconn.close();
+		}
+
+		return null;
+	}
+
+}
