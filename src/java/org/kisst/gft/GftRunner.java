42ee78de7543b0acdd5538ddfff71e71aecd75b6
Mark Hooijkaas <mark@kisst.org>
2017-02-20 09:02:40 +0000

    moved duo specific startup code from generic part

--- src/java/org/kisst/gft/GftRunner.java
+++ src/java/org/kisst/gft/GftRunner.java
@@ -1,21 +1,11 @@
 package org.kisst.gft;
 
-import nl.duo.gft.algemeen.GftDuoAlgemeenModule;
-import nl.duo.gft.chesicc.ChesiccModule;
-import nl.duo.gft.filetransfer.DuoFileTransferModule;
-import nl.duo.gft.klaarzettenBestand.KlaarzettenBestandModule;
-import nl.duo.gft.vzub.VzubModule;
 import org.kisst.gft.filetransfer.FileTransferModule;
 
 public class GftRunner extends BaseRunner {
 	public GftRunner(String configfilename) {
 		super("gft", configfilename,
-				FileTransferModule.class,
-				DuoFileTransferModule.class,
-				GftDuoAlgemeenModule.class,
-				KlaarzettenBestandModule.class,
-				VzubModule.class,
-				ChesiccModule.class
+				FileTransferModule.class
 		);
 	}
 
