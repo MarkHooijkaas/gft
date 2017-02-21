fbc5e2fd5cf07a0f6b407078c68ce1f9bf39c78d
Mark Hooijkaas <mark@hooijkaas.net>
2017-02-21 11:29:16 +0100

    count messages in QueueStatus instead of relying om MQ specific hacks

--- src/main/java/org/kisst/gft/admin/status/QueueStatus.java
+++ src/main/java/org/kisst/gft/admin/status/QueueStatus.java
@@ -1,66 +1,63 @@
 package org.kisst.gft.admin.status;
 
-import java.io.PrintWriter;
-
-import org.kisst.gft.GftContainer;
 import org.kisst.gft.GftWrapper;
 import org.kisst.gft.admin.ListenerServlet;
+import org.kisst.jms.JmsUtil;
 import org.kisst.jms.MultiListener;
-import org.kisst.mq.QueueManager;
 
-import com.ibm.mq.MQException;
-import com.ibm.mq.MQQueue;
-import com.ibm.mq.MQQueueManager;
-import com.ibm.mq.constants.MQConstants;
-
+import javax.jms.*;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 
 public class QueueStatus extends StatusItem {
-	private final MQQueueManager qmgr;
-	private final String name;
-	private final String baseName;
+	private final String queuename;
 	private final String url;
 	private final MultiListener listener;
 	
 	public QueueStatus(GftWrapper wrapper, MultiListener listener, String queuename) {
 		super(wrapper, listener.getQueue(queuename).substring(listener.getQueue(queuename).lastIndexOf('/')+1));
 		this.url=listener.getName()+"/"+queuename;
-		this.name=queuename;
-		qmgr=listener.getQueueSystem().createMQQueueManager();
+		this.queuename=queuename;
 		this.listener=listener;
-		QueueManager qm = new QueueManager(qmgr);
-		MQQueue q1 = qm.getQueue(listener.getQueue(queuename), MQConstants.MQOO_INQUIRE);
-		this.baseName=qm.getTargetQueue(q1);
-		
 	}
 
-	public String getUrl() { return url; } 
-
-	public int depth() {
-		if (qmgr==null)
-			return -1;
-    	int openOptions = MQConstants.MQOO_INQUIRE;
-    	MQQueue q;
-    	try {
-    		q = qmgr.accessQueue(this.baseName, openOptions);
-    		if (q.getQueueType() ==MQConstants.MQQT_ALIAS) {
-    			return -1;
-    		}
-    		else
-    		return q.getCurrentDepth();
-    	} catch (MQException e) { e.printStackTrace(); return -1; } // Ignore
-    }
+	public String getUrl() { return url; }
 
 	@Override public void writeDetails(PrintWriter out) {
 		super.writeDetails(out);
-		ListenerServlet.writeQueueMessages(out, listener, name);
+		ListenerServlet.writeQueueMessages(out, listener, queuename);
 	}
 
-	
-	
 	@Override public void refresh() {
-		this.problemCount=depth();
+		this.problemCount=countMessages();
 	}
 
 	@Override public String getMessage() { return problemCount+" messages in queue"; }
+
+	public int countMessages() {
+		Session session=null;
+		try {
+			session = listener.getQueueSystem().getConnection().createSession(true, Session.AUTO_ACKNOWLEDGE);
+			String q=listener.getQueue(queuename);
+
+			Queue destination = session.createQueue(q);
+			QueueBrowser browser = session.createBrowser(destination);
+			Enumeration<?> e = browser.getEnumeration();
+			int count=0;
+			while (e.hasMoreElements()) {
+				count++;
+				Message msg = (Message) e.nextElement();
+			}
+			return count;
+		}
+		catch (JMSException e) { throw JmsUtil.wrapJMSException(e); }
+		finally {
+			try {
+				if (session!=null)
+					session.close();
+			}
+			catch (JMSException e) { throw JmsUtil.wrapJMSException(e); }
+		}
+	}
 }
